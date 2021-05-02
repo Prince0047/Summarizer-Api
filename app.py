817a75014ee0d76829073bf4eba88de64c9a22b2
Prince0047 <sonubhadana838@gmail.com>
2021-05-02 11:55:02 +0530

    added upload path

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import flask
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, send_file, send_from_directory, session
 from flask_cors import CORS, cross_origin
+
+import os
+import glob
+from werkzeug.utils import secure_filename
 
 import spacy
 from textblob import TextBlob
@@ -12,8 +16,16 @@ from gensim.summarization import summarize
 # tokenizer = PegasusTokenizer.from_pretrained(model_name)
 # model = PegasusForConditionalGeneration.from_pretrained(model_name)
 
-
+UPLOAD_FOLDER = 'uploads/'
+UPLOAD_FOLDER2 = 'uploads2/'
+#app = Flask(__name__)
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER2'] = UPLOAD_FOLDER2
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 30
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 CORS(app)
 
@@ -45,6 +57,105 @@ def Sum():
 @app.route('/model')
 def abs_model():
     return render_template("working.htm")
+
+
+
+
+# Upload API
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Removing Files
+        Pfiles = glob.glob('uploads/*')
+        for f in Pfiles:
+            os.remove(f)
+
+        Pfiles2 = glob.glob('uploads2/*')
+        for f in Pfiles2:
+            os.remove(f)
+        req = request.form
+        # check if the post request has the file part
+        if req['form-name'] == 'form1':
+            text_area = req['txtarea']
+            percent = int(req['percent'])
+            percent = percent/100
+            print(percent)
+            sum_text = summarize(text_area, percent)
+            return render_template('upload_file.html', in_text=text_area, sum_text=sum_text)
+        if 'file' not in request.files:
+            print('no file')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+        else:
+            filename = secure_filename(file.filename)
+
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            text = " ".join((line for line in open(os.path.join(app.config['UPLOAD_FOLDER'], filename), encoding='utf-8')))
+            # os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            percent = int(req['percent'])
+            percent = percent/100
+            print(text)
+            text2 = summarize(text, percent)
+            print("after summary.....")
+
+            # file_path = app.config['UPLOAD_FOLDER2'] 
+            print(filename)
+            # filename= "SUM_" + filename
+            # files = open(file_path+ sum_file, 'w')
+            # files.write(text2)
+            # files.close()
+            with open(os.path.join(app.config['UPLOAD_FOLDER2'], filename), 'w') as f:
+                f.write(text2)
+            print("here is the filename : ",filename)
+
+            print("saved file successfully")
+      #send file name as parameter to downlad
+            # file_path = os.path.join(app.config['UPLOAD_FOLDER2'], filename)
+            # print("file for sent is ::: ")
+            # return send_file(file_path, as_attachment=True, attachment_filename='')
+            # file_path = os.path.join(app.config['UPLOAD_FOLDER2'], filename)
+
+            return redirect('/downloadfile/'+ filename)
+    return render_template('upload_file.html', in_text="Text area for input ...")
+# Download API
+@app.route("/downloadfile/<filename>", methods = ['GET'])
+def download_file(filename):
+    return render_template('download.html',value=filename)
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    print("file for sent is ::: ")
+    return send_from_directory(app.config['UPLOAD_FOLDER2'], filename=filename, as_attachment=True, attachment_filename="Sum_"+filename, cache_timeout=-1)
+
+
+@app.route("/delete" , methods = ['GET','POST'])
+def delete_Uploads():
+    if request.method == 'POST':
+        req = request.form
+        if req['username'] == "Prince0047" and req['password'] == "qwerty0047":
+
+            files = glob.glob('uploads/*')
+            for f in files:
+                os.remove(f)
+
+            files2 = glob.glob('uploads2/*')
+            for f in files2:
+                os.remove(f)
+            
+            
+            return "Data files have been deleted..."
+        
+        else:
+            return "Wrong Id or password"
+    
+
+    return render_template('delete.html')
+
 
 # Preflight Response
 
